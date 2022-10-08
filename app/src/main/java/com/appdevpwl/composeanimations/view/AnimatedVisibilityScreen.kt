d386b61d53e34b6b2cf5b862a3682e4ea730570d
Pawel Wlazlo <wlazlopwl@gmail.com>
2022-10-08 11:48:30 +0200

    update AnimatedVisibilityScreen.kt

--- app/src/main/java/com/appdevpwl/composeanimations/view/AnimatedVisibilityScreen.kt
+++ app/src/main/java/com/appdevpwl/composeanimations/view/AnimatedVisibilityScreen.kt
@@ -1,11 +1,16 @@
 package com.appdevpwl.composeanimations.view
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
 
@@ -15,6 +20,38 @@ fun AnimatedVisibilityScreen() {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
+        var visible by remember { mutableStateOf(true) }
+        val density = LocalDensity.current
+        AnimatedVisibility(
+            visible = visible,
+            enter = slideInVertically {
+                // Slide in from 40 dp from the top.
+                with(density) { -40.dp.roundToPx() }
+            } + expandVertically(
+                // Expand from the top.
+                expandFrom = Alignment.Top
+            ) + fadeIn(
+                // Fade in with the initial alpha of 0.3f.
+                initialAlpha = 0.5f
+            ),
+            exit = fadeOut()
+        ) {
+            Box(modifier = Modifier.width(60.dp).height(30.dp).background(Color.Gray))
+            Text(
+                "Hello",
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+        }
+
+        Button(
+            onClick = { visible = !visible },
+            modifier = Modifier.wrapContentSize(),
+//            colors = ButtonDefaults.textButtonColors(Color.White)
+        ) {
+            Text(text = "Animuj")
+        }
     }
 }
 
