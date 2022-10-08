49d5eeace9eeb4a9358c17ee6224b94dbe4ed863
Pawel Wlazlo <wlazlopwl@gmail.com>
2022-10-08 13:30:20 +0200

    update CrossfadeScreen.kt

--- app/src/main/java/com/appdevpwl/composeanimations/view/CrossfadeScreen.kt
+++ app/src/main/java/com/appdevpwl/composeanimations/view/CrossfadeScreen.kt
@@ -1,11 +1,64 @@
 package com.appdevpwl.composeanimations.view
 
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun CrossfadeScreen() {
+
+    var state by remember {
+        mutableStateOf(false)
+    }
+
+    Column {
+        Box {
+
+            Crossfade(
+                targetState = state,
+                animationSpec = tween(3000, easing = FastOutSlowInEasing)
+            ) {
+                when (it) {
+                    false -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp)
+                                .background(Color.Yellow)
+                        )
+
+                    }
+                    true -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp)
+                                .background(Color.Green)
+                        )
+                    }
+                }
+
+            }
+
+        }
+        StartAnimateButton {
+            state = !state
+        }
+    }
+
 }
+
+
 
 
 
