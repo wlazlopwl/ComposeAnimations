ab10f5537ff908a19568c30b5b618b14e8f5bc39
Pawel Wlazlo <wlazlopwl@gmail.com>
2022-10-23 14:38:28 +0200

    changing AnimatedVisibilityScreen.kt

--- app/src/main/java/com/appdevpwl/composeanimations/view/AnimatedVisibilityScreen.kt
+++ app/src/main/java/com/appdevpwl/composeanimations/view/AnimatedVisibilityScreen.kt
@@ -2,13 +2,13 @@ package com.appdevpwl.composeanimations.view
 
 import androidx.compose.animation.*
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
 
@@ -19,22 +19,14 @@ fun AnimatedVisibilityScreen() {
         modifier = Modifier.fillMaxSize()
     ) {
         var visible by remember { mutableStateOf(true) }
-        val density = LocalDensity.current
+
         AnimatedVisibility(
             visible = visible,
-            enter = slideInVertically {
-                with(density) { -40.dp.roundToPx() }
-            } + expandVertically(
-                expandFrom = Alignment.Top
-            ) + fadeIn(
-                initialAlpha = 0.5f
-            ),
-            exit = fadeOut()
+            enter = fadeIn() + slideInHorizontally(),
+            exit = fadeOut() + slideOutVertically ()
         ) {
             Box(
                 modifier = Modifier
-                    .width(60.dp)
-                    .height(30.dp)
                     .background(Color.Gray)
             )
         }
